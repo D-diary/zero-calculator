45c42079a866e93b0184d20a07c5b31e1f59ee9c
D-diary <dlrlsud@naver.com>
2022-04-26 15:56:03 +0900

    클릭 이벤트 구현

--- practice/script.js
+++ practice/script.js
@@ -8,4 +8,62 @@
   const getAll = (target) => {
     return document.querySelectorAll(target)
   }
+
+  class Calculator {
+    constructor(element) {
+      this.element = element
+      this.currentValue = ''
+      this.prevValue = ''
+      this.operation = null
+    } 
+
+    appendNumber(number) {
+      if (number === '.' && this.currentValue.includes('.')) return
+      this.currentValue = this.currentValue.toString() + number.toString()
+    }
+    setOpertaion(operation) {
+      this.resetOperation()
+      this.operation = operation
+      this.prevValue = this.currentValue
+      this.currentValue = ''
+
+      const elements = Array.from(getAll('.operation'))
+      const element = elements.filter((element) => element.innerText.includes(operation))[0]
+
+      element.classList.add('active')
+    }
+    updateDisplay() {
+      if (this.currentValue) {
+        this.element.value = this.currentValue
+        return
+      }
+    }
+    resetOperation() {
+      this.operation = null
+      const elements = Array.from(getAll('.operation'))
+      elements.forEach((element) => {
+        element.classList.remove('active')
+      })
+    }
+  }
+
+  const numberButtons = getAll('.cell_button.number')
+  const operationButtons = getAll('.cell_button.operation')
+  const display = get('.display')
+
+  const calculator = new Calculator(display)
+
+  numberButtons.forEach(button => {
+    button.addEventListener('click',() => {
+      calculator.appendNumber(button.innerText)
+      calculator.updateDisplay()
+    })
+  })
+
+  operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.setOpertaion(button.innerText)
+      calculator.updateDisplay()
+    })
+  })
 })()
